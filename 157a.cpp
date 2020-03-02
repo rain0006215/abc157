74887d34b15cf4c35a37214237d0e586758152ec
rain0006215 <rain0006215@gmail.com>
2020-03-03 00:45:16 +0900

    add A

---
+++ 157a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+int main(){
+    int N;
+    cin >> N;
+    int ans;
+    ans = N/2;
+    ans += N%2;
+    cout << ans << endl;
+ 
+}
